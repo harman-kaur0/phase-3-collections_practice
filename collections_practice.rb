96d2738a18516930f8c2d344a3aa529fa93de026
Harmandeep Kaur <harmandeepk628@gmail.com>
2021-05-17 16:20:44 -0700

    commit tree 35e99d87a5b94a842d319ba34c8c97f962b0293d

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
+def sort_array_asc(arr)
+    arr.sort do |a, b|
+        a <=> b
+    end
+end
 
+
+def sort_array_desc(arr)
+    arr.sort do |b, a|
+        b <=> a
+    end
+end
+
+def sort_array_char_count
+
+end
+
+def swap_elements
+
+end
+
+def reverse_array
+
+end
+
+def kesha_maker
+
+end
+
+def find_a
+
+end
+
+def sum_array
+
+end
+
+def add_s
+
+end
